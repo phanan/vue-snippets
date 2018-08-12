5cccffab72e01ddba624da76dd63e6c076272cbf
An Phan <an.phan@internations.org>
2018-08-12 17:52:09 +0200

    Fix sw scope

---
+++ themes/vuesnippets/static/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'vue-snippets-cache-v2'
+const urlsToCache = [
+  '/'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)))
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => {
+    if (response) {
+      return response
+    }
+
+    const fetchRequest = event.request.clone()
+
+    return fetch(fetchRequest).then(response => {
+        if(!response || response.status !== 200 || response.type !== 'basic') {
+          return response
+        }
+
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()))
+
+        return response
+      }
+    )
+  }))
+})
