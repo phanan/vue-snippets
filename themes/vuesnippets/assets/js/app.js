b4797e2d5ea029cb05f28877aeb2c69147f33658
An Phan <an.phan@internations.org>
2018-08-12 17:50:36 +0200

    Fix sw scope

--- themes/vuesnippets/assets/js/app.js
+++ themes/vuesnippets/assets/js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/js/sw.js').then(registration => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
       console.log('ServiceWorker registration successful with scope: ', registration.scope)
     }, err => {
       console.log('ServiceWorker registration failed: ', err)
